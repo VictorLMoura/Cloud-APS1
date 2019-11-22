804c3b55efc0f605b36e355cdbba8f3f6ee31f5d
victorlmoura <victorlaperutademoura@hotmail.com>
2019-11-22 10:49:06 -0300

    ajustando para con remota

--- aps1/instance/config.py
+++ aps1/instance/config.py
@@ -1,4 +1,4 @@
 # instance/config.py
 
 SECRET_KEY = 'p9Bv<3Eid9%$i01'
-SQLALCHEMY_DATABASE_URI = 'mysql://victor:Vlm1998!!!@localhost/aps1'
+SQLALCHEMY_DATABASE_URI = 'mysql://victor:Vlm1998!!!@3.86.151.123:localhost/aps1'
