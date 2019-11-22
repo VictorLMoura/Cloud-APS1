e87835a43a473a7ed32e9ed5ccda10b9129a6777
victorlmoura <victorlaperutademoura@hotmail.com>
2019-11-22 15:16:12 -0300

    ajuste

--- aps1/instance/config.py
+++ aps1/instance/config.py
@@ -1,4 +1,4 @@
 # instance/config.py
 
 SECRET_KEY = 'p9Bv<3Eid9%$i01'
-SQLALCHEMY_DATABASE_URI = 'mysql://victor:Vlm1998!!!@3.86.151.123/aps1'
+SQLALCHEMY_DATABASE_URI = 'mysql://victor:Vlm1998!!!@54.159.57.132/aps1'
