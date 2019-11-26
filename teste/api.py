a6b309820426397121add72ac04f81b8366fdc68
victorlmoura <victorlaperutademoura@hotmail.com>
2019-11-26 18:39:43 -0300

    voltando ao normal

--- teste/api.py
+++ teste/api.py
@@ -12,11 +12,11 @@ def lista_tarefas():
     """
     if request.method == 'POST':
         dic_temp["texto"] = request.json["texto"]
-        r = requests.post("http://" + os.environ['IPSERVER'] + ":8080/tarefa", json=dic_temp)
+        r = requests.post("http://" + os.environ['IPSERVER'] + ":5000/tarefa", json=dic_temp)
         return (r.text, r.status_code, r.headers.items())
 
     if request.method == 'GET':
-        r = requests.get("http://" + os.environ['IPSERVER'] + ":8080/tarefa")
+        r = requests.get("http://" + os.environ['IPSERVER'] + ":5000/tarefa")
         return (r.text, r.status_code, r.headers.items())
 
     return "404"
@@ -28,15 +28,15 @@ def altera_tarefas(id):
     """
     if request.method == 'PUT':
         dic_temp["texto"] = request.json["texto"]
-        r = requests.put("http://" + os.environ['IPSERVER'] + ":8080/tarefa/"+id, json=dic_temp)
+        r = requests.put("http://" + os.environ['IPSERVER'] + ":5000/tarefa/"+id, json=dic_temp)
         return (r.text, r.status_code, r.headers.items())
     
     if request.method == 'DELETE':
-        r = requests.delete("http://"+ os.environ['IPSERVER'] +":8080/tarefa/"+id)
+        r = requests.delete("http://"+ os.environ['IPSERVER'] +":5000/tarefa/"+id)
         return (r.text, r.status_code, r.headers.items())
     
     return "404"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=5000)
