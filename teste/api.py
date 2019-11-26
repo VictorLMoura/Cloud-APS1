437b7b5ec542b51470032b8c4b3bef6f4a7d4533
victorlmoura <victorlaperutademoura@hotmail.com>
2019-11-26 16:33:06 -0300

    simplificando api

---
+++ teste/api.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, redirect
+import requests
+import json
+import os
+
+app = Flask(__name__)
+
+@app.route('/tarefa', methods=['GET', 'POST'])
+def lista_tarefas():
+    """
+    Lista as tarefas ja criadas
+    """
+    if request.method == 'POST':
+        dic_temp["texto"] = request.json["texto"]
+        r = requests.post("http://" + os.environ['IPSERVER'] + ":5000/tarefa", json=dic_temp)
+        return (r.text, r.status_code, r.headers.items())
+
+    if request.method == 'GET':
+        r = requests.get("http://" + os.environ['IPSERVER'] + ":5000/tarefa")
+        return (r.text, r.status_code, r.headers.items())
+
+    return "404"
+
+@app.route('/tarefa/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def altera_tarefas(id):
+    """
+    Altera uma tarefa
+    """
+    if request.method == 'PUT':
+        dic_temp["texto"] = request.json["texto"]
+        r = requests.put("http://" + os.environ['IPSERVER'] + ":5000/tarefa/"+id, json=dic_temp)
+        return (r.text, r.status_code, r.headers.items())
+    
+    if request.method == 'DELETE':
+        r = requests.delete("http://"+ os.environ['IPSERVER'] +":5000/tarefa/"+id)
+        return (r.text, r.status_code, r.headers.items())
+    
+    return "404"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ teste/runner.sh
@@ -0,0 +1,2 @@
+export FLASK_APP=/home/ubuntu/Cloud-APS1/teste/api.py
+/usr/local/bin/flask run
