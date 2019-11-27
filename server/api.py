c02b967d4597e35bca32d941e6e1464791eb9e7a
victorlmoura <victorlaperutademoura@hotmail.com>
2019-11-26 23:56:47 -0300

    problemas acredito no libssl-dev, mudando aps1

---
+++ server/api.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, redirect, jsonify
+import requests
+import json
+import os
+import pymysql
+
+app = Flask(__name__)
+conn = pymysql.connect(host=os.environ['IPDB'], user='victor', password='Vlm1998!!!', db='aps1', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+
+conn.autocommit(True)
+
+def post_one(conn)
+
+@app.route('/tarefa', methods=['GET', 'POST'])
+def lista_tarefas():
+    """
+    Lista as tarefas ja criadas
+    """
+    if request.method == 'POST':
+        with conn.cursor() as cursor:
+            try:
+                cursor.execute('INSERT INTO tarefas (texto) VALUES (%s)', (request.json["texto"]))
+                conn.commit()
+                conn.close()
+            except pymysql.err.IntegrityError as e:
+                raise ValueError(f'Não posso inserir {request.json["texto"]} na tabela')
+                conn.close()
+        return "Algo errado"
+
+    if request.method == 'GET':
+        with conn.cursor() as cursor:
+            try:
+                cursor.execute('SELECT * FROM tarefas')
+                res = cursor.fetchall()
+                conn.close()
+                return res
+            except pymysql.err.IntegrityError as e:
+                raise ValueError(f'ERRO')
+                conn.close()
+                return -1
+        return "Algo errado"
+
+    return "Algo errado"
+
+@app.route('/tarefa/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def altera_tarefas(id):
+    """
+    Altera uma tarefa
+    """    
+    if request.method == 'DELETE':
+        with conn.cursor() as cursor:
+            cursor.execute('DELETE FROM tarefas WHERE id=%s', (id))
+            conn.close()
+        return "Uso: /tarefa /tarefa/<int:id>"
+    
+    return "Uso: /tarefa /tarefa/<int:id>"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
---
+++ server/runner.sh
@@ -0,0 +1,2 @@
+export FLASK_APP=/home/ubuntu/Cloud-APS1/server/api.py
+/usr/local/bin/flask run
