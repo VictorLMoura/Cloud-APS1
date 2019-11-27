42800cfd2a44d0924247963e8ffc3b11e76d7285
victorlmoura <victorlaperutademoura@hotmail.com>
2019-11-27 14:00:42 -0300

    tentando

--- aps1/runner.sh
+++ aps1/runner.sh
@@ -1,4 +1,4 @@
 export FLASK_CONFIG=development
 export FLASK_APP=/home/ubuntu/Cloud-APS1/aps1/run.py
 /usr/local/bin/flask db init
-/usr/local/bin/flask run
+/usr/local/bin/flask run --host=0.0.0.0
--- server/runner.sh
+++ server/runner.sh
@@ -1,2 +1,2 @@
 export FLASK_APP=/home/ubuntu/Cloud-APS1/server/api.py
-/usr/local/bin/flask run
+/usr/local/bin/flask run --host=0.0.0.0
